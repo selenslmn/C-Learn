e1bbcecb666550eb7ad9a0b6d770aef071c668ee
selenslmn <56594127+selenslmn@users.noreply.github.com>
2022-07-12 20:42:38 +0300

    Update Sample3.c

--- Sample3.c
+++ Sample3.c
@@ -4,19 +4,18 @@ int main(void)
 {
 	int x = 10, y = 20;
 
-	printf("x= %d, y = %d\n", x,y);
+	printf("x= %d, y = %d\n", x,y);			/* x= 10, y = 20 */
 
-	printf("x= %d, y = %d\n", x + 2, y);
+	printf("x= %d, y = %d\n", x + 2, y);		/* x= 12, y = 20 */
 
-	printf("%d%d\n", x, y);
+	printf("%d%d\n", x, y);				/* 1020 */
 
-	printf("%dx%d\n", x, y);
+	printf("%dx%d\n", x, y);			/* 10x20 */
 
-	printf("%d-%d\n", x, y);
+	printf("%d-%d\n", x, y);			/* 10-20 */
 
-	printf("%d\n%d", x, y);
-
-	printf("%d\n%d\n", x, y);
+	printf("%d\n%d", x, y);				/* 10
+                                                           20 */
 	
 	return 0;
 }
