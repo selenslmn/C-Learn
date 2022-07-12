b63529657647f33b66cbfe5539c9eace2425d50a
selenslmn <56594127+selenslmn@users.noreply.github.com>
2022-07-12 20:54:40 +0300

    Update Sample5.c

--- Sample5.c
+++ Sample5.c
@@ -9,7 +9,7 @@ main(void)
 	int d = 1234567;
 
 	printf("%-20d%f\n", a, sqrt(a));	/* 10       3.162278 */
-	printf("%-20d%f\n", b, sqrt(b));	/* 7		   2.645751 */
+	printf("%-20d%f\n", b, sqrt(b));	/* 7	    2.645751 */
 	printf("%-20d%f\n", c, sqrt(c));	/* 121      11.000000 */
 	printf("%-20d%f\n", d, sqrt(d));	/* 123456   1111.110706 */
 
