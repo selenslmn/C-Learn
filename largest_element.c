3b5c0fc17e65aad1680aa7977dc3e932c09f00a6
selenslmn <56594127+selenslmn@users.noreply.github.com>
2022-10-24 20:32:01 +0300

    Update largest_element.c

--- largest_element.c
+++ largest_element.c
@@ -19,3 +19,29 @@ int main(void)
 
     return 0;
 }
+
+-----------------------------------------------------------------------
+    
+#include <stdio.h>
+
+int getmax(int *pi, size_t size)
+{
+    int max = pi[0];
+
+    for (size_t i = 1; i < size; ++i)
+        if (pi[i] > max)
+            max = pi[i];
+
+    return max;
+}
+
+int main(void)
+{
+    int a[10] = {45, 23, 11, 67, 21, 7, 32, 76, 22, 47};
+    int max;
+
+    max = getmax(a, 10);
+    printf("%d\n", max);
+
+    return 0;
+}
