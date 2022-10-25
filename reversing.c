19fbfdbd36be8f5f4b2199a6501608aa9fc96b7c
selenslmn <56594127+selenslmn@users.noreply.github.com>
2022-10-25 12:44:40 +0300

    Update reversing.c

--- reversing.c
+++ reversing.c
@@ -21,3 +21,35 @@ int main(void)
 
     return 0;
 }
+
+---------------------------------------------------------------------------------------
+    
+#include <stdio.h>
+
+void reverse(int *pi, size_t size);
+
+int main(void)
+{
+    int a[10] = {2, 56, 11, 1, 58, 23, 32, 43, 67, 15};
+
+    for (size_t i = 0; i < 10; ++i)
+        printf("%d ", a[i]);
+    printf("\n");
+
+    reverse(a, 10);
+
+    for (size_t i = 0; i < 10; ++i)
+        printf("%d ", a[i]);
+    printf("\n");
+
+    return 0;
+}
+
+void reverse(int *pi, size_t size)
+{
+    for (size_t i = 0; i < size / 2; ++i) {
+        int temp = pi[i];
+        pi[i] = pi[size - 1 - i];
+        pi[size - 1 - i] = temp;
+    }
+}
