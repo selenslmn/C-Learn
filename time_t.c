00d62b8998684a973996ecf05364964b0c18dfe7
Selen Salman <56594127+selenslmn@users.noreply.github.com>
2023-01-17 12:02:47 +0300

    Update time_t.c

--- time_t.c
+++ time_t.c
@@ -13,3 +13,25 @@ int main(void)
 
 	return 0;
 }
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+// In the example below, first the number of seconds elapsed from the epoch is found with the time function.
+// And then converted into date-time information with the localtime function.
+
+#include <stdio.h>
+#include <time.h>
+
+int main(void)
+{
+	time_t t;
+	struct tm *pt;
+	char *days[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+	t = time(NULL);
+	pt = localtime(&t);
+
+	printf("%02d/%02d/%04d %02d:%02d:%02d - %s\n", pt->tm_mday, pt->tm_mon + 1, pt->tm_year + 1900, pt->tm_hour, pt->tm_min, pt->tm_sec, days[pt->tm_wday]);
+
+	return 0;
+}
